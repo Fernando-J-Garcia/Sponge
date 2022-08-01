532bcd86d28c288a2fec8162c466c5ab455a213e
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-07-31 17:53:15 -0700

    fixed resize issues and can now change bg color

--- components/Rect.tsx
+++ components/Rect.tsx
@@ -1,22 +1,16 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useContext } from "react";
+import { CanvasContext } from "./CanvasProvider";
 
 export default function Rect() {
+  const { setFocusedElement, currentColor } = useContext(CanvasContext);
   const ref = useRef<HTMLDivElement>(null);
+  const backgroundColor = useRef(currentColor);
   useEffect(() => {
     document.addEventListener("mousemove", handleMouseMove);
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
-  const style = {
-    width: "100px",
-    height: "100px",
-    top: "0px",
-    left: "0px",
-    position: "absolute",
-    backgroundColor: "red",
-    resize: "both",
-  };
   let offsetLeft = 0;
   let offsetTop = 0;
   function handleMouseMove(e: MouseEvent) {
@@ -26,24 +20,34 @@ export default function Rect() {
     if (offsetLeft === 0) offsetLeft = e.clientX - parseInt(rectStyle.left);
     if (offsetTop === 0) offsetTop = e.clientY - parseInt(rectStyle.top);
 
-    console.log(e.clientX, parseInt(rectStyle.left));
+    //console.log(e.clientX, parseInt(rectStyle.left));
     ref.current.style.left = e.clientX - offsetLeft + "px";
     ref.current.style.top = e.clientY - offsetTop + "px";
   }
   function handleMouseUp() {
     document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
     offsetLeft = 0;
     offsetTop = 0;
   }
-  function handleMouseDown() {
+  function handleMouseDown(e) {
+    document.addEventListener("mouseup", handleMouseUp);
+    if (ref.current === null) return;
+    const position = ref.current.getBoundingClientRect();
+    console.log({ mouseX: e.clientX });
+    console.log({ squareX: position.right });
+    if (position.right < e.clientX + 20 && position.top < e.clientY + 20)
+      return;
+
     document.addEventListener("mousemove", handleMouseMove);
   }
   return (
     <div
-      style={style}
-      onMouseUp={handleMouseUp}
+      //onMouseUp={handleMouseUp}
       ref={ref}
       onMouseDown={handleMouseDown}
+      data-element="rect"
+      style={{ backgroundColor: backgroundColor.current }}
     >
       Rect
     </div>
