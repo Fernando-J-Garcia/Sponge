b064364a55a16d9bcd505ed652a0320fc69c3dcf
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-08-01 10:03:31 -0700

    moved mouse events to context

--- components/CanvasProvider.tsx
+++ components/CanvasProvider.tsx
@@ -1,12 +1,21 @@
 import { jsx } from "@emotion/react";
 import { prepareServerlessUrl } from "next/dist/server/base-server";
-import React, { createContext, FC, useContext, useRef, useState } from "react";
+import React, {
+  createContext,
+  FC,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 interface CanvasContextInterface {
   elements: JSX.Element[];
   addElement: (element: JSX.Element) => void;
   setElements: (value: JSX.Element[]) => void;
+  elementPropertiesList: HTMLDivElement[];
+  addElementProperty: (value: HTMLDivElement) => void;
   focusedElement: HTMLDivElement | null;
-  setFocusedElement: (element: HTMLDivElement) => void;
+  setFocusedElement: (element: HTMLDivElement | null) => void;
   currentColor: string;
   setCurrentColor: (value: string) => void;
 }
@@ -16,6 +25,8 @@ const defaultValue = {
     console.log("default");
   },
   setElements: () => {},
+  elementPropertiesList: [],
+  addElementProperty: () => {},
   focusedElement: null,
   setFocusedElement: () => {},
   currentColor: "#000000",
@@ -27,19 +38,58 @@ export const CanvasContext =
 export function CanvasProvider({ children }: any) {
   const [elements, setElements] = useState<JSX.Element[]>([]);
   const focusedElement = useRef<HTMLDivElement | null>(null);
+  const [elementPropertiesList, setElementPropertiesList] = useState<
+    HTMLDivElement[]
+  >([]);
   const currentColor = useRef("#000000");
+
+  useEffect(() => {
+    document.addEventListener("mouseup", handleMouseUp);
+    document.addEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  //MOUSE EVENTS=========================================================================
+  let offsetLeft = 0;
+  let offsetTop = 0;
+
+  function handleMouseMove(e) {
+    if (focusedElement.current === null) return;
+    const rectStyle = getComputedStyle(focusedElement.current);
+
+    if (offsetLeft === 0) offsetLeft = e.clientX - parseInt(rectStyle.left);
+    if (offsetTop === 0) offsetTop = e.clientY - parseInt(rectStyle.top);
+
+    //console.log(e.clientX, parseInt(rectStyle.left));
+    focusedElement.current.style.left = e.clientX - offsetLeft + "px";
+    focusedElement.current.style.top = e.clientY - offsetTop + "px";
+  }
+  function handleMouseUp() {
+    focusedElement.current = null;
+    offsetLeft = 0;
+    offsetTop = 0;
+  }
+  //End Mouse Events ====================================================================
 
   function addElement(element: JSX.Element) {
     console.log(element);
     setElements((prev) => [...prev, element]);
   }
-  function setFocusedElement(element: HTMLDivElement) {
+  function setFocusedElement(element: HTMLDivElement | null) {
     focusedElement.current = element;
+    console.log(focusedElement.current);
+    if (focusedElement.current === null) return;
+    focusedElement.current.addEventListener("mouesmove", handleMouseMove);
   }
+  function addElementProperty(value: HTMLDivElement) {
+    setElementPropertiesList((prev) => [...prev, value]);
+  }
+
   const value: CanvasContextInterface = {
     elements: elements,
     addElement: addElement,
     setElements: (value: JSX.Element[]) => setElements(value),
+    elementPropertiesList: elementPropertiesList,
+    addElementProperty: addElementProperty,
     focusedElement: focusedElement.current,
     setFocusedElement: setFocusedElement,
     currentColor: currentColor.current,
--- components/Rect.tsx
+++ components/Rect.tsx
@@ -2,50 +2,34 @@ import React, { useRef, useEffect, useContext } from "react";
 import { CanvasContext } from "./CanvasProvider";
 
 export default function Rect() {
-  const { setFocusedElement, currentColor } = useContext(CanvasContext);
+  const { setFocusedElement, currentColor, addElementProperty } =
+    useContext(CanvasContext);
   const ref = useRef<HTMLDivElement>(null);
+  const onMount = useRef(true);
   const backgroundColor = useRef(currentColor);
   useEffect(() => {
-    document.addEventListener("mousemove", handleMouseMove);
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-  let offsetLeft = 0;
-  let offsetTop = 0;
-  function handleMouseMove(e: MouseEvent) {
+    if (onMount.current === false) {
+      return;
+    }
+    onMount.current = false;
+
     if (ref.current === null) return;
-    const rectStyle = getComputedStyle(ref.current);
-
-    if (offsetLeft === 0) offsetLeft = e.clientX - parseInt(rectStyle.left);
-    if (offsetTop === 0) offsetTop = e.clientY - parseInt(rectStyle.top);
-
-    //console.log(e.clientX, parseInt(rectStyle.left));
-    ref.current.style.left = e.clientX - offsetLeft + "px";
-    ref.current.style.top = e.clientY - offsetTop + "px";
-  }
-  function handleMouseUp() {
-    document.removeEventListener("mousemove", handleMouseMove);
-    document.removeEventListener("mouseup", handleMouseUp);
-    offsetLeft = 0;
-    offsetTop = 0;
-  }
+    addElementProperty(ref.current);
+    setFocusedElement(ref.current);
+  }, []);
   function handleMouseDown(e) {
-    document.addEventListener("mouseup", handleMouseUp);
     if (ref.current === null) return;
     const position = ref.current.getBoundingClientRect();
     console.log({ mouseX: e.clientX });
     console.log({ squareX: position.right });
     if (position.right < e.clientX + 20 && position.top < e.clientY + 20)
       return;
-
-    document.addEventListener("mousemove", handleMouseMove);
+    setFocusedElement(ref.current);
   }
   return (
     <div
-      //onMouseUp={handleMouseUp}
-      ref={ref}
       onMouseDown={handleMouseDown}
+      ref={ref}
       data-element="rect"
       style={{ backgroundColor: backgroundColor.current }}
     >
