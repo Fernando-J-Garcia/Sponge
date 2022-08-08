cc5afcfbff7e3c72c0e01aede4f4743b742365a4
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-08-07 17:10:56 -0700

    got rid of react warnings

--- components/svgs/RotateIcon.tsx
+++ components/svgs/RotateIcon.tsx
@@ -18,8 +18,8 @@ export default function RotateIcon({
     >
       <path
         d="M0 0v8h8V7H5c0-2.21-1.79-4-4-4V0H0zm1 4v3h3c0-1.657-1.343-3-3-3z"
-        fill-rule="evenodd"
-        fill-opacity="1"
+        fillRule="evenodd"
+        fillOpacity="1"
         fill={color || "black"}
         stroke="none"
       />
