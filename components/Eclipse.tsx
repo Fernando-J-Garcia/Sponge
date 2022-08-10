6a77cfd57d92c700957cb3a8708b3cc9527cfe64
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-08-10 14:04:28 -0700

    added eclipse

---
+++ components/Eclipse.tsx
@@ -0,0 +1,35 @@
+import React, { useRef, useEffect, useContext } from "react";
+import { CanvasContext } from "./CanvasProvider";
+
+export default function Eclipse() {
+  const { setFocusedElement, currentColor, addElementProperty } =
+    useContext(CanvasContext);
+  const ref = useRef<HTMLDivElement>(null);
+  const onMount = useRef(true);
+  const backgroundColor = useRef(currentColor);
+  useEffect(() => {
+    if (onMount.current === false) {
+      return;
+    }
+    onMount.current = false;
+
+    if (ref.current === null) return;
+    addElementProperty(ref.current);
+    setFocusedElement(ref.current);
+  }, []);
+  function handleMouseDown(e) {
+    if (ref.current === null) return;
+    const position = ref.current.getBoundingClientRect();
+    if (position.right < e.clientX + 20 && position.top < e.clientY + 20)
+      return;
+    setFocusedElement(ref.current);
+  }
+  return (
+    <div
+      onMouseDown={handleMouseDown}
+      ref={ref}
+      data-element="rect"
+      style={{ backgroundColor: backgroundColor.current }}
+    ></div>
+  );
+}
