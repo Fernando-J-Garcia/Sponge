371f44e2cc33faa4fa8fd1fa332cf6e1d5f190c2
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-08-26 02:49:14 -0700

    Added new logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,37 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Sponge</title>
+        <meta name="description" content="Tool for creating art with CSS" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
